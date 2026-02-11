378d585ab7440d8816680fd92fdb3fba21a658eb
Jana Mahmoud <23-101109@students.eui.edu.eg>
2026-02-11 14:22:14 +0200

    starting second column

---
+++ src/components/column2.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Column2 = () => {
+    return ( 
+        <>
+        <div className="profiles">
+            
+        </div>
+        </>
+     );
+}
+ 
+export default Column2;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './home.css';
 import NavBar from '../components/navbar';
 import Column1 from '../components/column1';
+import Column2 from '../components/column2';
 
 const Home = () => {
     return ( 
@@ -9,6 +10,7 @@ const Home = () => {
         <NavBar />
         <main>
             <Column1 />
+            <Column2 />
         </main>
         </>
      );
